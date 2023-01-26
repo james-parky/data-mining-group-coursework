1b8f18500d98cdd3a018de870d39777a72688e6b
james-parky <72101291+james-parky@users.noreply.github.com>
2023-01-26 17:22:21 +0000

    Fixed overfitting due to including the scores as a feature

--- support_vector_regression.py
+++ support_vector_regression.py
@@ -19,17 +19,22 @@ def predict(training_data: str, test_set: str) -> None:
     test_set = pd.read_csv(test_set)
     dataset = dataset.replace(['Lose', 'Draw', 'Win'],[-1,0,1])
     test_set = test_set.replace(['Lose', 'Draw', 'Win'],[-1,0,1])
-    #numerical_columns = [c for c in dataset.columns if dataset[c].dtype in [np.float64, np.int64]]
-    #print(numerical_columns)
+    numerical_columns = [c for c in dataset.columns if dataset[c].dtype in [np.float64, np.int64]]
     dataset = dataset.select_dtypes([np.number])
     test_set = test_set.select_dtypes([np.number])
+    print(f'Original dataset columns: {list(dataset.columns)}')
 
-    if training_data not in ['reconstructed_match_history_with_interpolation.csv', 'reconstructed_with_lr_df.csv']:
+    if training_data == 'dataset/match_history.csv':
         missing_rows = list(dataset.isnull().any(axis=1))
         for index, row in dataset.iterrows():
             if missing_rows[index] == True:
                 dataset = dataset.drop(index)
+    if training_data == 'reconstructed_match_history_with_interpolation.csv':
+        dataset = dataset.copy().drop(['home_team_total_fifa_points', 'away_team_total_fifa_points', 'away_team_score'],axis=1)
+    if training_data == 'reconstructed_with_lr_df.csv':
+        dataset = dataset.copy().drop(['date'],axis=1)
 
+    print(f'Edited dataset columns: {list(dataset.columns)}')
     #print(x_column_names)
     #x_data = dataset[['date,home_team,away_team,home_team_continent,away_team_continent,home_team_fifa_rank,away_team_fifa_rank,home_team_total_fifa_points,away_team_total_fifa_points,home_team_score,away_team_score,tournament,city,country,neutral_location,shoot_out,home_team_result,home_team_goalkeeper_score,away_team_goalkeeper_score,home_team_mean_defense_score,home_team_mean_offense_score,home_team_mean_midfield_score,away_team_mean_defense_score,away_team_mean_offense_score,away_team_mean_midfield_score']]
     
@@ -41,11 +46,11 @@ def predict(training_data: str, test_set: str) -> None:
     svm_model = make_pipeline(StandardScaler(), SVC(kernel='poly')).fit(x_train, y_train)
     svm_predictions = svm_model.predict(x_test)
 
-    lr_model = LogisticRegression(multi_class='multinomial', solver='newton-cg', max_iter=10000).fit(x_train, y_train)
+    lr_model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=100000).fit(x_train, y_train)
     lr_predictions = lr_model.predict(x_test)
 
     from sklearn.neighbors import KNeighborsClassifier
-    knn_model = KNeighborsClassifier(n_neighbors=5).fit(x_train, y_train)
+    knn_model = KNeighborsClassifier(n_neighbors=40).fit(x_train, y_train)
     knn_predictions = knn_model.predict(x_test)
 
     from sklearn.neural_network import MLPClassifier
