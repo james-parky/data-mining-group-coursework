e9214c4fd7685ba94231f1722e1f866f3263613a
james-parky <72101291+james-parky@users.noreply.github.com>
2023-01-27 00:03:48 +0000

    Fixed column removal to accommodate new features.

--- support_vector_regression.py
+++ support_vector_regression.py
@@ -1,10 +1,3 @@
-'''
-What is a Support Vector Machine?
-    The objective of the support vector machine algorithm is to find a
-    hyperplane in an N dimensional space that distinctly classifies the data
-    points.
-'''
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,25 +12,26 @@ from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precisio
 def predict(training_data: str) -> None:
     dataset = pd.read_csv(training_data)
     dataset = dataset.replace(['Lose', 'Draw', 'Win'],[-1,0,1])
-    # numerical_columns = [c for c in dataset.columns if dataset[c].dtype in [np.float64, np.int64]]
     dataset = dataset.select_dtypes([np.number])
-    # test_set = test_set.select_dtypes([np.number])
     print(f'Original dataset columns: {list(dataset.columns)}')
 
-    if training_data == 'dataset/match_history.csv':
-        missing_rows = list(dataset.isnull().any(axis=1))
-        for index, row in dataset.iterrows():
-            if missing_rows[index] == True:
-                dataset = dataset.drop(index)
-    if training_data == 'reconstructed_match_history_with_interpolation.csv':
-        dataset = dataset.copy().drop(['home_team_total_fifa_points', 'away_team_total_fifa_points', 'away_team_score'],axis=1)
-    if training_data == 'reconstructed_with_lr_df.csv':
-        dataset = dataset.copy().drop(['date'],axis=1)
-
+    active_features = ['home_team_fifa_rank', 'away_team_fifa_rank',
+                       'home_team_score', 'home_team_result',
+                       'home_team_goalkeeper_score',
+                       'away_team_goalkeeper_score',
+                       'home_team_mean_defense_score',
+                       'home_team_mean_offense_score',
+                       'home_team_mean_midfield_score',
+                       'away_team_mean_defense_score',
+                       'away_team_mean_offense_score',
+                       'away_team_mean_midfield_score',
+                       'home_team_total_average','away_team_total_average',
+                       'goal_difference','home_team_rank_difference']
+    for column in dataset.columns:
+        if column not in active_features:
+            dataset = dataset.copy().drop([column], axis=1)
     print(f'Edited dataset columns: {list(dataset.columns)}')
-    #print(x_column_names)
-    #x_data = dataset[['date,home_team,away_team,home_team_continent,away_team_continent,home_team_fifa_rank,away_team_fifa_rank,home_team_total_fifa_points,away_team_total_fifa_points,home_team_score,away_team_score,tournament,city,country,neutral_location,shoot_out,home_team_result,home_team_goalkeeper_score,away_team_goalkeeper_score,home_team_mean_defense_score,home_team_mean_offense_score,home_team_mean_midfield_score,away_team_mean_defense_score,away_team_mean_offense_score,away_team_mean_midfield_score']]
-    
+
     from sklearn.linear_model import LogisticRegression
     #return np.mean(lr_predictions == y_test_lr.values)*100
 
@@ -57,10 +51,10 @@ def predict(training_data: str) -> None:
     mlp_model = MLPClassifier(random_state=1, max_iter=10000).fit(x_train, y_train)
     mlp_predictions = mlp_model.predict(x_test)
 
-    print(f'LR Predictions: {np.mean(lr_predictions == y_test.values)*100}')
-    print(f'SVM Predictions: {np.mean(svm_predictions == y_test.values)*100}')
-    print(f'KNN Predictions: {np.mean(knn_predictions == y_test.values)*100}')
-    print(f'MLP Predictions: {np.mean(mlp_predictions == y_test.values)*100}')
+    print(f'\033[96mLR Predictions: {np.mean(lr_predictions == y_test.values)*100} \033[0m')
+    print(f'\033[96mSVM Predictions: {np.mean(svm_predictions == y_test.values)*100} \033[0m')
+    print(f'\033[96mKNN Predictions: {np.mean(knn_predictions == y_test.values)*100} \033[0m')
+    print(f'\033[96mMLP Predictions: {np.mean(mlp_predictions == y_test.values)*100} \033[0m')
 
     
     # Plotting confusion matrices
@@ -76,18 +70,6 @@ def predict(training_data: str) -> None:
         print(all_model_names[i] + "F1 score: " + str(f1_score(y_test, all_predictions[i], average="macro")))
         print(all_model_names[i] + "Precision score: " + str(precision_score(y_test, all_predictions[i], average="macro")))
         print(all_model_names[i] + "Recall score: " + str(recall_score(y_test, all_predictions[i], average="macro")))
-    '''    
-    rows = []
-    for index, row in dataset.iterrows():
-        rows.append(row)
-        for item in row:
-            if item in [None, '']:
-                print('missing data')
-    '''
-    #dataset['home_team_result'] = dataset['home_team_result'].replace(['Lose', 'Draw', 'Win'], [-1, 0, 1])
-
-    #dataset.to_numpy()
-    #print(dataset)
 
 
 if __name__ == '__main__':
@@ -100,4 +82,4 @@ if __name__ == '__main__':
     print('Linear Regression with new features:')
     predict('dataset/linear_feature_match_history.csv')
     print('Gaussian with new features:')
-    predict('gaussian_feature_match_history.csv')
+    predict('dataset/gaussian_feature_match_history.csv')
